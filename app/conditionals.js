352fda466d85a30a089f98561e8589cfeb1f6808
G0SSZILLA <gossman15@gmail.com>
2020-03-16 17:40:19 -0600

    1st push

--- app/conditionals.js
+++ app/conditionals.js
@@ -1,8 +1,11 @@
 //1. Given two numbers, write a function that will return  the larger number
 
 function largerNum(num1, num2) {
-    // you code here
-
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
 }
 
 // 2. Given two numbers, amount correct and amount possible of a test, return the corresponding letter grade.
@@ -14,7 +17,20 @@ function largerNum(num1, num2) {
 // output: "F"
 
 function testGrader(score, possible) {
-
+    if (score / possible >= .9) {
+        return 'A'
+    }
+    if (score / possible >= .8) {
+        return 'B'
+    }
+    if (score / possible >= .7) {
+        return 'C'
+    }
+    if (score / possible >= .6) {
+        return 'D'
+    } else {
+        return 'F'
+    }
 }
 
 
@@ -29,12 +45,36 @@ function testGrader(score, possible) {
 //Make sure your ranges are inclusive
 
 function timeOfDayGreeting(hour) {
-    // you code below
+    if (hour >= 5 && hour <= 11) {
+        return 'good morning'
+    }
+    if (hour >= 12 && hour <= 17) {
+        return 'good afternoon'
+    }
+    if (hour >= 18 && hour <= 21) {
+        return 'good evening'
+    }
+    if (hour >= 22 && hour <= 24) {
+        return 'good night'
+    } else if (hour >= 1 && hour <= 4) {
+        return 'good night'
+    }
+
+
 
 }
 
 //4. Write a function that will take in a number and return 'fever' if it indicates a fever (over 98.6) and additionally if the person should go to the hospital (at or above 103) 'fever go to hospital' (hint: try this with string concatenation), if it is under return 'no fever'
 function isFever(temp) {
+    if (temp > 98.6 && temp < 103) {
+        return 'fever'
+    }
+    if (temp >= 103) {
+        return 'fever go to hospital'
+    }
+    if (temp <= 98.6) {
+        return 'no fever'
+    }
 
 }
 
@@ -47,16 +87,24 @@ let myCar = {
 }
 
 function isStopped(car) {
-
+    if (car.moving == false) {
+        return true
+    } else {
+        return false
+    }
 }
 
 //6. Write a function that returns true if a dish is yours and is dirty, or false if one of the statements is false
 
 let dish = {
     yourDish: true,
-    isDirty: true
+    isDirty: true,
 }
 
 function washDish(dish) {
-
+    if (dish.yourDish == true && dish.isDirty == true) {
+        return true
+    } else {
+        return false
+    }
 }
--- app/objects.js
+++ app/objects.js
@@ -6,30 +6,34 @@
 // boolean value, and the likes property should have a value that is an 
 // array.Inside the array, include at least 4 values.
 let goodDoggo = {
-  //your code in here
+    name: 'doggo',
+    legs: 5,
+    tail: true,
+    likes: ['dog food', 'bones', 'cuddles', 'sleeping']
 }
+
 
 
 // write a function that returns the value of the legs property on your dog object using dot notation.
 function howManyLegs() {
-  // your code inside here
-  // don't forget to use the return keyword
+    // your code inside here
+    // don't forget to use the return keyword
 }
 
 // Cat Challenges
 
 let cat = {
-  name: "Pickles",
-  legs: 4,
-  whiskers: true,
-  likes: ["napping", "playing", "running at 2:00am", "pets", "stealing food off the counter"]
+    name: "Pickles",
+    legs: 4,
+    whiskers: true,
+    likes: ["napping", "playing", "running at 2:00am", "pets", "stealing food off the counter"]
 }
 
 // Write a function that uses bracket notation to change the cat's name from 
 // "Pickles" to "Kevin".Then use either dot or bracket notation to return 
 // the value of the name property.
 function renameCat() {
-  // your code inside here
+    // your code inside here
 }
 
 // Create a function that uses dot or bracket notation to create a property 
@@ -37,7 +41,7 @@ function renameCat() {
 // Then return the value of the color property.
 
 function colorTheCat() {
-  // your code inside here
+    // your code inside here
 }
 
 // Delete the whiskers property off of the cat object. You may use either dot or bracket notation.
@@ -48,5 +52,5 @@ function colorTheCat() {
 // Example: objectName.property[indexNumber]
 
 function returnIndex(cat) {
-  // your code inside here
+    // your code inside here
 }
